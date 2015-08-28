d0ed76943820673c72150db87ba575b63fd9dbc0
Bailey <baileypiggott@gmail.com>
2015-08-28 11:40:43 -0400

    update colours to be consistent with engineering

--- psyc_value_linegraph.R
+++ psyc_value_linegraph.R
@@ -2,35 +2,35 @@
 # psyc VALUE line graph
 source("set_up.R")
 
-# choose VALUE criteria:
+# choose VALUE criteria ------------------------
 
 learning = ps
 legend = c("Define Problem", "Identify Strategies", "Propose Solutions", "Evaluate Potential Solutions", "Implement Solutions", "Evaluate Outcomes")
-col = rev(brewer.pal(8,"Oranges"))
+col = rev(brewer.pal(8,"Greens"))
 shape_vals = c(0,1,2,15,16,17) #values for scale_shape_manual
 graph_title = "Problem Solving"
 
 learning = ct
 legend = c("Explanation of Issues", "Evidence", "Influence of Context and Assumptions", "Student's Position", "Conclusions") 
-col = rev(brewer.pal(7,"Greens")) #colour for scale_colour_manual
+col = rev(brewer.pal(7,"Blues")) #colour for scale_colour_manual
 line_vals = c(5,4,3,2,1) #values for linetype
 shape_vals = c(0,1,2,15,16) #values for scale_shape_manual
 graph_title = "Critical Thinking"
 
 learning = wc  
 legend = c("Context and Purpose for Writing", "Content Development", "Genre and Disciplinary Conventions", "Sources and Evidence", "Syntax and Mechanics") 
-col = rev(brewer.pal(7,"Blues"))
+col = rev(brewer.pal(7,"Oranges"))
 shape_vals = c(0,1,2,15,16) #values for scale_shape_manual
 graph_title = "Written Communication"
 
 learning = summary #summary of the 3 outcomes  
 legend = c("Problem Solving", "Critical Thinking", "Written Communication")
-col = c("#FF8833" , "#33CC44", "#3388EE")
+col = c("#33CC44","#3388EE", "#FF8833"  )
 shape_vals = c(15,16, 17) #values for scale_shape_manual
 graph_title = "Learning Outcomes"
 
 
-# plot:
+# CREATE PLOT ----------------------------------
 
 ggplot(
   data = learning, 
@@ -50,6 +50,7 @@ ggplot(
     panel.background = element_rect("white"),
     axis.ticks.x = element_blank(), # remove x axis ticks
     plot.title = element_text(size = 15),
+    legend.key = element_blank(),
     axis.title.x = element_blank(), # remove x axis title
     axis.title.y = element_text(size = 14),
     axis.text = element_text(size = 12) #size of x axis labels
